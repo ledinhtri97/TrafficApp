be9cc3e0b1354d6efe8d1f68db852b7dbd94e7e3
Le Dinh Tri <ledinhtri.cse.hcmut@gmail.com>
2019-07-24 01:10:44 +0700

    add icon gvlab

--- dev/GVComponents/GVNavBar.js
+++ dev/GVComponents/GVNavBar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
 
 import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
 // import Traffic from '@material-ui/icons/Traffic';
 
 const GVNarBarStyles = theme => ({
@@ -11,7 +12,14 @@ const GVNarBarStyles = theme => ({
 		height: '10%',
 		textAlign: 'center',
 		color: "#FFFFFF",
-	}
+		display: 'flex',
+		justifyContent: 'center',
+	},
+	logo: {
+    	marginRight: 5,
+    	width: 60,
+    	height: 60,
+  	},
 });
 
 class GVNarBar extends React.Component {
@@ -25,6 +33,7 @@ class GVNarBar extends React.Component {
 
 		return (
 			<div className={classes.root}>
+			<Avatar alt="GVLab" src="./static/imgs/gvlab-icon.png" className={classes.logo} />
 			<Typography align="center" variant="h4" style={{paddingTop: '12px'}}>
           		GVLab | Demo Traffic Application
         	</Typography>
